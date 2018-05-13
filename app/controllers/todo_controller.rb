6075b5956a1fa712f19c77162d07d51b3614276b
Nancy Nnebe <nancyynnebe@gmail.com>
2018-05-13 15:28:28 +0000

    pull straight from the database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,28 +2,6 @@ class TodoController < ApplicationController
     def index 
     end
     def show
-        todo_id = params[:id]
-        if todo_id == '1'
-            @todo_description = "Make the curriculum"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id == '2'
-            @todo_description = "Buy workshop supplies"
-            @todo_pomodoro_estimate = 3
-        elsif todo_id == '3' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
-            @todo_description = "Meet with the volunteer trainers"
-            @todo_pomodoro_estimate = 4
-        elsif todo_id == '4'
-        	@todo_description = "Order food for Sunday and Saturday"
-        	@todo_pomodoro_estimate = 2
-        elsif todo_id == '5'
-        	@todo_description = "Get pre-work assignments"
-        	@todo_pomodoro_estimate = 2
-        elsif todo_id == '6'
-        	@todo_description = "Send workshop location to all students"
-        	@todo_pomodoro_estimate = 2
-        elsif todo_id == '7'
-        	@todo_description = "Have a great workshop"
-        	@todo_pomodoro_estimate = 2
-        end
+        @todo = Todo.find_by_id(params[:id])
     end
-end
+end 
